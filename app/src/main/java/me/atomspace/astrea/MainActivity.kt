a16e7112622f803982cd5c2cc5ee383dfff039ee
Oleg-Storozhev <olegStoroshev@gmail.com>
2020-07-26 16:02:34 +0300

    ss

--- app/src/main/java/me/atomspace/astrea/MainActivity.kt
+++ app/src/main/java/me/atomspace/astrea/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.atomspace.astrea
 
 import android.graphics.Color
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import lecho.lib.hellocharts.model.PieChartData
@@ -15,16 +16,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         pieChartView = findViewById(R.id.budget_chart)
+        val df = DecimalFormat("#.#")
+        val Schools = 500F
+        val Bisnes = 155F
+        val Hospitals = 520F
+        val Other = 52F
+
+        fun Summ() : Float {
+            return Schools + Bisnes + Hospitals + Other
+        }
+        fun Procent(a: Float) : Float{
+            return (a * 100) / Summ()
+        }
+        fun Check(Sum: Float): String{
+            when{
+                Sum >= 1000 && Sum <= 1000000 -> return "${df.format(Sum/1000)} Тыс"
+                Sum >= 1000000 && Sum <= 1000000000 -> return "${df.format(Sum/1000000)} Млн"
+                Sum >= 1000000000 -> return "${df.format(Sum/1000000000)} Млрд"
+                else -> return "${df.format(Sum)}"
+            }
+
+        }
+
 
         val data: ArrayList<SliceValue> = ArrayList()
-        data.add(SliceValue(50F, Color.CYAN).setLabel("Учебные заведения"))
-        data.add(SliceValue(50F, Color.BLUE).setLabel("Малый бизнес"))
-        data.add(SliceValue(45F, Color.YELLOW).setLabel("Больницы"))
-        data.add(SliceValue(5F, Color.GREEN).setLabel("Другое"))
+        data.add(SliceValue(Procent(Schools), Color.CYAN).setLabel("Учебные заведения"))
+        data.add(SliceValue(Procent(Bisnes), Color.BLUE).setLabel("Малый бизнес"))
+        data.add(SliceValue(Procent(Hospitals), Color.YELLOW).setLabel("Больницы"))
+        data.add(SliceValue(Procent(Other), Color.GREEN).setLabel("Другое"))
 
         val pieChartData = PieChartData(data)
         pieChartData.setHasLabels(true).valueLabelTextSize = 14
-        pieChartData.setHasCenterCircle(true).centerText1 = "400 млн"
+        pieChartData.setHasCenterCircle(true).centerText1 = "${Check(Summ())}"
         pieChartData.centerText1FontSize = 36
         pieChartView.pieChartData = pieChartData
     }
